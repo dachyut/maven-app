dc37c23cc0d2439e414441ff6bcf96e6dbfb126d
Achyut <adeshpande@carbonite.com>
2020-02-18 15:02:22 +0530

    initial checkin

---
+++ src/main/java/com/mycompany/app/App1.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class App1 {
+
+	public String Sample() {
+		return "Hello World";
+	}
+}
---
+++ src/test/java/com/mycompany/app/AppJunitTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppJunitTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
